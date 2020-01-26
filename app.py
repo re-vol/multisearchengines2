898034483e9be904305a3f32c28a4f96170bf17b
hirokazuniimoto <55869749+hirokazuniimoto@users.noreply.github.com>
2020-01-27 02:49:04 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask ,render_template ,url_for,request,redirect,session
+from flask.views import MethodView
+import webbrowser
+
+app=Flask(__name__)
+
+@app.route('/',methods=["GET"])
+def hello_world5():
+    return render_template('index8.html',title="Multi search engines",message="↓ Select your search engine")
+
+@app.route('/',methods=["POST"])
+def form6():
+    return render_template('index8.html',title="Multi search engines",message="↓ search again ?")
+
+@app.route('/secondpage',methods=["GET"])
+def hello_world6():
+    return render_template('index9.html',title="Multi search engines",message="↓ Select your SNS")
+
+@app.route('/secondpage',methods=["POST"])
+def form7():
+    return render_template('index9.html',title="Multi search engines",message="↓ search again ?")
+
+
+@app.route('/thirdpage',methods=["GET"])
+def hello_world7():
+    return render_template('index10.html',title="Multi search engines",message="↓ Select your Web Service")
+
+@app.route('/thirdpage',methods=["POST"])
+def form8():
+    return render_template('index10.html',title="Multi search engines",message="↓ search again ?")
+
+@app.route('/forthpage',methods=["GET"])
+def hello_world8():
+    return render_template('index11.html',title="Multi search engines",message="↓ Select your News site")
+
+@app.route('/forthpage',methods=["POST"])
+def form9():
+    return render_template('index11.html',title="Multi search engines",message="↓ search again ?")
+
+@app.route('/googlepp',methods=["GET"])
+def hello_world9():
+    return render_template('index13.html',title="G o o g l e + +",message="↓ Select your search style")
+
+@app.route('/googlepp',methods=["POST"])
+def form10():
+    return render_template('index13.html',title="G o o g l e + +",message="↓ search again ")
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.debug=True
+    app.run(host='localhost')
